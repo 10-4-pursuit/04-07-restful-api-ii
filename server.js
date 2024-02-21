85f918935b4550d507bd60b7b492bc1e3f986481
dttrue <danieltorres@pursuit.org>
2024-02-21 13:04:59 -0500

    Daniels restfu-api-2

---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require('./index');
+
+
+const port = process.env.PORT || 3000;
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
