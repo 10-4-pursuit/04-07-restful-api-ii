d557e3b64b25d34cb973d90bdd30525e8a1e3cbf
GyancarlosPinto <gyancarlospinto@pursuit.org>
2024-02-20 23:31:10 -0500

    Got should fetch a single customer by ID to pass

--- index.js
+++ index.js
@@ -20,8 +20,13 @@ app.get('/users', (req, res) => {
     res.json(users)
 });
 
-app.get('/users', (req, res) => {
-    
+app.get('/users/:id', (req, res) => {
+    const user = users.find(u => u.id == req.params.id);
+    if (user) {
+        res.status(200).json(user)
+    } else {
+        res.status(404).json("User not found")
+    }
 });
 
 app.put('/users', (req, res) => {
