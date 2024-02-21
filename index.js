c6191eb3ebc25270a52050c4c3dbdb941e2e62e1
GyancarlosPinto <gyancarlospinto@pursuit.org>
2024-02-20 23:13:41 -0500

    Got should list all customers to pass

--- index.js
+++ index.js
@@ -16,20 +16,20 @@ app.post('/users', (req, res) => {
     res.status(201).send(newUser)
 });
 
-// app.get('/users', (req, res) => {
-//     res.json(users)
-// });
+app.get('/users', (req, res) => {
+    res.json(users)
+});
 
-// app.get('/users', (req, res) => {
+app.get('/users', (req, res) => {
     
-// });
+});
 
-// app.put('/users', (req, res) => {
+app.put('/users', (req, res) => {
     
-// });
+});
 
-// app.delete('/users', (req, res) => {
+app.delete('/users', (req, res) => {
     
-// });
+});
 
 module.exports = app;
